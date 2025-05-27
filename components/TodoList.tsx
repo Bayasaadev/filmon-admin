01e2dc56e8fcdb4b5008338ee0d1f1fecff56424
Bayasaadev <bayasaadev@gmail.com>
2025-05-27 22:43:42 +0800

    2nd commit: Charts, CardList, TodoList

---
+++ components/TodoList.tsx
@@ -0,0 +1,69 @@
+"use client"
+
+import { useState } from "react"
+import { Card } from "./ui/card"
+import { Checkbox } from "./ui/checkbox"
+import { ScrollArea } from "./ui/scroll-area"
+import { Calendar } from "./ui/calendar"
+import { Popover, PopoverTrigger, PopoverContent } from "./ui/popover"
+import { Button } from "./ui/button"
+import { CalendarIcon } from "lucide-react"
+import { format } from "date-fns"
+
+const TodoList = () => {
+
+  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="">
+      <h1 className="text-lg font-medium mb-6">Todo List</h1>
+      <Popover open={open} onOpenChange={setOpen}>
+        <PopoverTrigger asChild>
+          <Button className="w-full">
+            <CalendarIcon />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="p-0 w-auto">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={(date) => {
+              setDate(date)
+              setOpen(false)
+            }}            
+          />
+        </PopoverContent>
+      </Popover>
+      {/* LIST */}
+      <ScrollArea className="max-h-[400px] mt-4 overflow-y-auto">
+        <div className="flex flex-col gap-4">
+          {/* ListItem */}
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" checked />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+            </div>
+          </Card>
+          {/* ListItem */}
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" checked />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+            </div>
+          </Card>
+          {/* ListItem */}
+          <Card className="p-4">
+            <div className="flex items-center gap-4">
+              <Checkbox id="item1" checked />
+              <label htmlFor="item1" className="text-sm text-muted-foreground">Lorem ipsum dolor sit amet consectetur adipisicing elit.</label>
+            </div>
+          </Card>
+        </div>
+      </ScrollArea>
+    </div>
+  )
+}
+
+export default TodoList
